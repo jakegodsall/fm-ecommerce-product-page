47d28d8c3c468cfd25b9cb079ed10fce09929c67
jakegodsall <jake.edward.godsall@gmail.com>
2023-03-07 10:54:12 +0100

    build desktop mode navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,33 @@ const Header = () => {
     return (
         <header className='flex justify-between p-4 items-center'>
             <div className='flex'>
-                <img src={MenuIcon} alt='open menu' className='mr-4 self-center' />
+                <img
+                    src={MenuIcon}
+                    alt='open menu'
+                    className='mr-4 self-center cursor-pointer sm:hidden'
+                />
                 <img src={Logo} alt='logo' className='' />
+                <nav className='hidden sm:block mx-8 md:mx-16 transition-all duration-1000'>
+                    <ul className='flex self-center gap-4 text-sm cursor-pointer'>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
             </div>
             <div className='flex items-center'>
-                <img src={CartIcon} alt='view cart' className='mr-4' />
-                <img src={DefaultAccountAvatar} alt='default account avatar' className='w-8' />
+                <img
+                    src={CartIcon}
+                    alt='view cart'
+                    className='mr-4 cursor-pointer md:mr-8 transition-all duration-1000'
+                />
+                <img
+                    src={DefaultAccountAvatar}
+                    alt='default account avatar'
+                    className='w-8 cursor-pointer sm:w-12 transition-all duration-1000'
+                />
             </div>
         </header>
     );
