b725d562eede823a3726b6926aa09a359c7b4b1c
jakegodsall <jake.edward.godsall@gmail.com>
2023-03-09 11:12:21 +0100

    fixed functionality of navbar modal

--- src/components/NavModal.js
+++ src/components/NavModal.js
@@ -5,18 +5,20 @@ import CloseIcon from '../assets/icons/icon-close.svg';
 
 const NavModal = ({ navOpen, handleNav }) => {
     return ReactDOM.createPortal(
-        <div
-            className={
-                navOpen
-                    ? 'visible bg-[#000]/70 absolute left-0 right-0 top-0 bottom-0 transition-all duration-500 z-10'
-                    : 'invisible opacity-0 absolute left-0 right-0 top-0 bottom-0 transition-all duration-1000 z-10'
-            }
-        >
+        <React.Fragment>
+            <div
+                onClick={() => handleNav()}
+                className={
+                    navOpen
+                        ? 'visible bg-[#000]/70 fixed left-0 right-0 top-0 bottom-0 transition-all duration-500 z-10'
+                        : 'invisible opacity-0 fixed left-0 right-0 top-0 bottom-0 transition-all duration-1000 z-10'
+                }
+            ></div>
             <div
                 className={
                     navOpen
-                        ? 'w-[60%] h-full bg-[#fff] p-6 opacity-100 transition-all duration-1000'
-                        : 'w-[60%] h-full bg-[#fff] p-6 opacity-100 translate-x-[-100%] transition-all duration-1000'
+                        ? 'fixed top-0 bottom-0 left-0 right-[40%] h-full bg-[#fff] p-6 opacity-100 transition-all duration-1000 z-20'
+                        : 'fixed top-0 bottom-0 left-0 right-[40%] h-full bg-[#fff] p-6 opacity-100 translate-x-[-100%] transition-all duration-1000 z-20'
                 }
             >
                 <img
@@ -35,7 +37,8 @@ const NavModal = ({ navOpen, handleNav }) => {
                     </ul>
                 </nav>
             </div>
-        </div>,
+        </React.Fragment>,
+
         document.getElementById('nav-portal')
     );
 };
