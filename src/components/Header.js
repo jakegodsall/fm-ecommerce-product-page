c55b6514dcf8175d1cc1a2182f1f1599fa34fa31
jakegodsall <jake.edward.godsall@gmail.com>
2024-01-09 20:43:40 +0000

    extracted NavBar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import NavBar from "./NavBar";
 import NavModal from "./Modals/NavModal";
 import CartModal from "./Modals/CartModal";
 
@@ -32,25 +33,8 @@ const Header = (props) => {
             onClick={handleNavOpen}
           />
           <img src={Logo} alt="logo" className="cursor-pointer" />
-          <nav className="mx-8 hidden h-full transition-all duration-1000 sm:block md:mx-16">
-            <ul className="flex cursor-pointer gap-8 self-center text-sm text-[#b6bcc8]">
-              <li className="border-b-4 border-orange border-opacity-0 py-8 transition-all duration-500 hover:border-opacity-100 hover:text-[#000] active:text-[#000]">
-                Collections
-              </li>
-              <li className="border-b-4 border-orange border-opacity-0 py-8 transition-all duration-500 hover:border-opacity-100 hover:text-[#000] active:text-[#000]">
-                Men
-              </li>
-              <li className="border-b-4 border-orange border-opacity-0 py-8 transition-all duration-500 hover:border-opacity-100 hover:text-[#000] active:text-[#000]">
-                Women
-              </li>
-              <li className="border-b-4 border-orange border-opacity-0 py-8 transition-all duration-500 hover:border-opacity-100 hover:text-[#000] active:text-[#000]">
-                About
-              </li>
-              <li className="border-b-4 border-orange border-opacity-0 py-8 transition-all duration-500 hover:border-opacity-100 hover:text-[#000] active:text-[#000]">
-                Contact
-              </li>
-            </ul>
-          </nav>
+
+          <NavBar />
         </div>
         <div className="flex items-center">
           <div
